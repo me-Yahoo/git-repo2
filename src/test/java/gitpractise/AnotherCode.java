cd1686b2f6f9a57c81bd84bebf79f613559f3e2a
me-Yahoo <meguru0209@yahoo.com>
2022-12-15 15:34:07 +0530

    first commit - gitlatest

---
+++ src/test/java/gitpractise/AnotherCode.java
@@ -0,0 +1,10 @@
+package gitpractise;
+
+public class AnotherCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/seleniumDataDrivenpkg/GitClass1.java
@@ -0,0 +1,17 @@
+package seleniumDataDrivenpkg;
+
+public class GitClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public static void test() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+}
